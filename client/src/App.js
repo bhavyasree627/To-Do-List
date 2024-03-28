7101dff918fb1a1f47ce8299e0afe61515a1f7cc
Bhavya <bhogathibhavya99@gmail.com>
2024-03-28 18:52:47 +0530

    Implemented CRUD operations completed TODOlist app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import CSS file
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState([]);
+  const [newtask, setNewtask] = useState('');
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/gettask").then(
+      arr => setItem(arr.data)
+    )
+  }, []);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/addtask", { todo: newtask }).then(
+      arr => setItem(arr.data)
+    )
+    setNewtask(''); // Clear input field after submission
+  }
+
+  const deleteHandler = id => {
+    axios.delete(`http://localhost:5000/delete/${id}`).then(
+      arr => setItem(arr.data)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <center>
+        <h1>To-Do List</h1>
+        <form onSubmit={submitHandler}>
+          <input 
+            type="text" 
+            value={newtask}
+            onChange={(e) => setNewtask(e.target.value)}
+            placeholder="Enter a new task" 
+          />
+          <button id="add" type="submit">Add Task</button>
+        </form>
+        <div className="task-list">
+          {item.map(task =>
+            <div className="task-item" key={task._id}>
+              <h2>{task.todo}</h2>
+              <button onClick={() => deleteHandler(task._id)}>Delete Task</button>
+            </div>
+          )}
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const TaskSchema = require('./model');
+const cors = require('cors');
+
+mongoose.connect("mongodb+srv://bhavya627:bhavya627@cluster0.9lczmvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+    ()=>console.log("DB Connected")
+);
+
+const app = express();
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json());
+app.get('/', (req, res) => { 
+    res.send("Hello World");
+});
+
+app.post('/addtask', async (req, res) => {
+    const {todo} = req.body;
+    try {
+        const newData = new TaskSchema({
+            todo: todo
+        });
+        await newData.save();
+        return res.json(await TaskSchema.find())
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get('/gettask', async (req, res) => {
+    try {
+        return res.json(await TaskSchema.find());
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        await TaskSchema.findByIdAndDelete(req.params.id)
+        return res.json(await TaskSchema.find())
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(5000,()=> console.log("Server running...."));
